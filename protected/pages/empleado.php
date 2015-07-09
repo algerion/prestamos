71fb02c259a345a97f6142ea6d82d18cee25b1ad
algerion <orctek-varios@yahoo.com.mx>
2015-07-09 16:08:31 -0500

    Lectura de dbf empleado y rellenado de tabla
    Lee el dbf emplearh.dbf, extrae sus datos, y rellena la tabla de
    empleados con ella.

--- protected/pages/empleado.php
+++ protected/pages/empleado.php
@@ -1,4 +1,8 @@
 <?php
+include_once('../compartidos/clases/leedbf.php');
+include_once('../compartidos/clases/charset.php');
+include_once('../compartidos/clases/conexion.php');
+
 class Empleado extends TPage
 {
 	var $dbConexion;
@@ -7,14 +11,32 @@ class Empleado extends TPage
 	{
 		parent::onLoad($param);
 		
-		//$dsn = "DRIVER={Microsoft dBase Driver (*.dbf)};SourceType=DBF;SourceDB=C:\\db\\article.dbf;DefaultDir=$excelDir;Exclusive=NO;collate=Machine;NULL=NO;DELETED=NO;BACKGROUNDFETCH=NO;";
-		$dsn = "Driver={Microsoft Visual FoxPro Driver};SourceType=DBF;SourceDB=temp/EMPLEARH.dbf;Exclusive=NO;Collate=Machine;NULL=NO;DELETED=NO;BACKGROUNDFETCH=NO;OLE DB Services=0";
-		$this->dbConexion = odbc_connect($dsn,"","");
-		$result = odbc_exec($this->conn, "SELECT * FROM emplearh");
-		if ($valor = odbc_fetch_array($result))
+		$this->dbConexion = Conexion::getConexion($this->Application, "dbpr");
+		Conexion::createConfiguracion();
+
+		$regs = LeeDBF::registros_dbf("temp/EMPLEARH.dbf");
+		foreach($regs as $r)
 		{
-			foreach($valor as $v)
-				echo $v;
+			$consulta = "insert into empleados values (:numero, :nombre, :paterno, :materno, :ura, :ur, :pr)";
+			$comando = $this->dbConexion->createCommand($consulta);
+			$comando->bindValue(":numero", $r["NUMERO"]);
+			$comando->bindValue(":nombre", $r["NOMBRE"]);
+			$comando->bindValue(":paterno", $r["PATERNO"]);
+			$comando->bindValue(":materno", $r["MATERNO"]);
+			$comando->bindValue(":ura", $r["URA"]);
+			$comando->bindValue(":ur", $r["UR"]);
+			$comando->bindValue(":pr", $r["PR"]);
+			$comando->execute();
+
+
+//echo $consulta . $r["NUMERO"] . "<br />";			
+//var_dump($r);
+/*			foreach($r as $key=>$value)
+			{
+				echo $key . ": " . Charset::CambiaCharset($value, 'CP850', 'UTF-8') . " ";
+			echo "<br />";
+			}
+*/
 		}
 	}
 }
