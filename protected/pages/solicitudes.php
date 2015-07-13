bb1f196f14e31da8f376a2d752ab6c6f3b1af9eb
algerion <orctek-varios@yahoo.com.mx>
2015-07-13 09:39:34 -0500

    Revert "este es el abance del proyecto"
    This reverts commit bc759a0fd486accf9ebb4778d226fab52df43894.

---
+++ protected/pages/solicitudes.php
@@ -0,0 +1,26 @@
+<?php
+//Prado::using('System.Util.*'); //TVarDump
+/*Prado::using('System.Web.UI.ActiveControls.*');
+include_once('../compartidos/clases/listas.php');
+include_once('../compartidos/clases/conexion.php');
+include_once('../compartidos/clases/envia_mail.php');
+include_once('../compartidos/clases/charset.php');
+*/
+
+class solicitudes extends TPage
+{
+	var $dbConexion;
+
+	public function onLoad($param)
+	{
+		parent::onLoad($param);
+//		$this->dbConexion = Conexion::getConexion($this->Application, "dbpr");
+//		Conexion::createConfiguracion();
+/*		if(!$this->IsPostBack)
+		{
+		}
+*/
+	}
+}
+
+?>
