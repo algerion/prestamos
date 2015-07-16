bb996e89fb3935c11417c788910cb4400808b068
itandehui <itapad@hotmail.com>
2015-07-16 15:42:29 -0500

    cambios de base de datos
    cambios de base de datos

--- prestamos.sql
+++ prestamos.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
+-- version 4.3.11
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-07-2015 a las 21:11:06
--- Versión del servidor: 5.6.16
--- Versión de PHP: 5.5.11
+-- Tiempo de generación: 16-07-2015 a las 22:41:37
+-- Versión del servidor: 5.6.24
+-- Versión de PHP: 5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,18 +23,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `bitacora`
+--
+
+DROP TABLE IF EXISTS `bitacora`;
+CREATE TABLE IF NOT EXISTS `bitacora` (
+  `id_regisrto` int(11) NOT NULL,
+  `fechahora` date NOT NULL,
+  `tabla` varchar(15) NOT NULL,
+  `archivo` varchar(15) NOT NULL,
+  `fechahora_archivo` date NOT NULL,
+  `longitud_archivo` int(1) NOT NULL,
+  `importe` varchar(1) NOT NULL,
+  `id_usuario` int(1) NOT NULL,
+  `estatus` varchar(15) NOT NULL,
+  `observaciones` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `catsindicato`
 --
 
 DROP TABLE IF EXISTS `catsindicato`;
 CREATE TABLE IF NOT EXISTS `catsindicato` (
-  `idSindicato` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identificador de la tabla \r\n',
+  `idSindicato` int(11) NOT NULL COMMENT 'Identificador de la tabla \r\n',
   `sindicato` varchar(50) DEFAULT ' ' COMMENT 'nombre del sindicato\r\n',
   `cveSindicato` varchar(5) DEFAULT '0' COMMENT 'Clave del Sindicato\r\n',
-  `representante` varchar(150) DEFAULT ' ' COMMENT 'nombre del representante del sindicato',
-  PRIMARY KEY (`idSindicato`),
-  UNIQUE KEY `idSindicato` (`idSindicato`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COMMENT='catÃ¡logo de Sindicatos			\r\n' AUTO_INCREMENT=14 ;
+  `representante` varchar(150) DEFAULT ' ' COMMENT 'nombre del representante del sindicato'
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1 COMMENT='catÃ¡logo de Sindicatos			\r\n';
 
 --
 -- Volcado de datos para la tabla `catsindicato`
@@ -63,7 +81,7 @@ INSERT INTO `catsindicato` (`idSindicato`, `sindicato`, `cveSindicato`, `represe
 
 DROP TABLE IF EXISTS `descuento`;
 CREATE TABLE IF NOT EXISTS `descuento` (
-  `id_descuento` int(11) NOT NULL AUTO_INCREMENT,
+  `id_descuento` int(11) NOT NULL,
   `origen` varchar(1) NOT NULL,
   `creado` datetime NOT NULL,
   `modificado` datetime NOT NULL,
@@ -73,9 +91,8 @@ CREATE TABLE IF NOT EXISTS `descuento` (
   `observaciones` text NOT NULL,
   `tipo` varchar(1) NOT NULL,
   `pago` varchar(1) NOT NULL,
-  `periodo` int(11) NOT NULL,
-  PRIMARY KEY (`id_descuento`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `periodo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -92,8 +109,7 @@ CREATE TABLE IF NOT EXISTS `empleados` (
   `sindicato` varchar(2) NOT NULL,
   `fec_ingre` date NOT NULL,
   `status` varchar(1) NOT NULL,
-  `tipo_nomi` int(1) NOT NULL,
-  PRIMARY KEY (`numero`)
+  `tipo_nomi` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -128,18 +144,18 @@ INSERT INTO `empleados` (`numero`, `nombre`, `paterno`, `materno`, `sindicato`, 
 
 DROP TABLE IF EXISTS `estatus`;
 CREATE TABLE IF NOT EXISTS `estatus` (
-  `id_estatus` int(11) NOT NULL AUTO_INCREMENT,
-  `estatus` varchar(50) NOT NULL,
-  PRIMARY KEY (`id_estatus`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `id_estatus` int(11) NOT NULL,
+  `estatus` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `estatus`
 --
 
 INSERT INTO `estatus` (`id_estatus`, `estatus`) VALUES
+(-1, 'error'),
 (1, 'generado'),
-(2, 'emviado'),
+(2, 'enviado'),
 (3, 'recibido'),
 (4, 'aplicado');
 
@@ -151,16 +167,15 @@ INSERT INTO `estatus` (`id_estatus`, `estatus`) VALUES
 
 DROP TABLE IF EXISTS `movimientos`;
 CREATE TABLE IF NOT EXISTS `movimientos` (
-  `id_movimiento` int(11) NOT NULL AUTO_INCREMENT,
+  `id_movimiento` int(11) NOT NULL,
   `id_contrato` int(11) NOT NULL,
   `num_movto` int(11) NOT NULL,
   `fecha` date NOT NULL,
   `movimiento` varchar(100) NOT NULL,
   `justificacion` varchar(500) NOT NULL,
   `cargo` decimal(12,2) NOT NULL,
-  `abono` decimal(12,2) NOT NULL,
-  PRIMARY KEY (`id_movimiento`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `abono` decimal(12,2) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `movimientos`
@@ -179,8 +194,7 @@ INSERT INTO `movimientos` (`id_movimiento`, `id_contrato`, `num_movto`, `fecha`,
 DROP TABLE IF EXISTS `parametros`;
 CREATE TABLE IF NOT EXISTS `parametros` (
   `llave` varchar(20) NOT NULL,
-  `valor` varchar(250) NOT NULL,
-  PRIMARY KEY (`llave`)
+  `valor` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -212,6 +226,24 @@ CREATE TABLE IF NOT EXISTS `pdfijarh` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `pensionados`
+--
+
+DROP TABLE IF EXISTS `pensionados`;
+CREATE TABLE IF NOT EXISTS `pensionados` (
+  `número` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `paterno` varchar(15) NOT NULL,
+  `materno` varchar(15) NOT NULL,
+  `sindicato` varchar(2) NOT NULL,
+  `fecha_ingre` date NOT NULL,
+  `estatus` varchar(1) NOT NULL,
+  `tipo_nomi` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `solicitud`
 --
 
@@ -233,11 +265,10 @@ CREATE TABLE IF NOT EXISTS `solicitud` (
 
 DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id_usuario` int(32) NOT NULL AUTO_INCREMENT,
+  `id_usuario` int(32) NOT NULL,
   `usuario` varchar(45) NOT NULL,
-  `acceso` varchar(32) NOT NULL,
-  PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `acceso` varchar(32) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -247,6 +278,81 @@ INSERT INTO `usuarios` (`id_usuario`, `usuario`, `acceso`) VALUES
 (1, 'prueba', '0e7881f0d44670fed326557fc047de90'),
 (2, 'admin', '7a95bf926a0333f57705aeac07a362a2');
 
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `catsindicato`
+--
+ALTER TABLE `catsindicato`
+  ADD PRIMARY KEY (`idSindicato`), ADD UNIQUE KEY `idSindicato` (`idSindicato`);
+
+--
+-- Indices de la tabla `descuento`
+--
+ALTER TABLE `descuento`
+  ADD PRIMARY KEY (`id_descuento`);
+
+--
+-- Indices de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  ADD PRIMARY KEY (`numero`);
+
+--
+-- Indices de la tabla `estatus`
+--
+ALTER TABLE `estatus`
+  ADD PRIMARY KEY (`id_estatus`);
+
+--
+-- Indices de la tabla `movimientos`
+--
+ALTER TABLE `movimientos`
+  ADD PRIMARY KEY (`id_movimiento`);
+
+--
+-- Indices de la tabla `parametros`
+--
+ALTER TABLE `parametros`
+  ADD PRIMARY KEY (`llave`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `catsindicato`
+--
+ALTER TABLE `catsindicato`
+  MODIFY `idSindicato` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identificador de la tabla \r\n',AUTO_INCREMENT=14;
+--
+-- AUTO_INCREMENT de la tabla `descuento`
+--
+ALTER TABLE `descuento`
+  MODIFY `id_descuento` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `estatus`
+--
+ALTER TABLE `estatus`
+  MODIFY `id_estatus` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT de la tabla `movimientos`
+--
+ALTER TABLE `movimientos`
+  MODIFY `id_movimiento` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(32) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
