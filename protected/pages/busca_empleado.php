56d1a5c628eae61f6ab7d66c5b1cf79c6839a8db
algerion <orctek-varios@yahoo.com.mx>
2015-07-24 12:00:29 -0500

    Consulta de búsqueda de empleado
    Generada la consulta para búsqueda de empleados y exhibición de los
    resultados de búsqueda

--- protected/pages/busca_empleado.php
+++ protected/pages/busca_empleado.php
@@ -21,32 +21,40 @@ class busca_empleado extends TPage
 	public function rellena_sindicatos()
 	{
 		$result = Conexion::Retorna_Registro($this->dbConexion, "catsindicatos", array("1"=>"1"));
-		$this->ddlSindicato->DataSource = $result;
+		$sindicatos = array_merge(array(array("cve_sindicato"=>"T", "sindicato"=>"Todos")), $result);
+		$this->ddlSindicato->DataSource = $sindicatos;
 		$this->ddlSindicato->dataBind();
 	}
 	
 	public function btnBuscar_Click($sender, $param)
 	{
-		$camposempjub = "SELECT e.numero, nombre, paterno, materno, cs.sindicato, fec_ingre, df.importe, df.porcentaje FROM ";
-		$joinsempjub = " e JOIN catsindicatos cs ON e.sindicato = cs.cve_sindicato JOIN descuentos_fijos df ON e.numero = df.numero WHERE df.concepto = 61";
-		$externos = "SELECT e.numero, nombre, paterno, materno, 0 AS sindicato, fec_ingre, 0 AS importe, 0 AS porcentaje FROM externos";
+		$camposempjub = "SELECT e.numero, nombre, paterno, materno, cs.sindicato, fec_ingre, IFNULL(df.importe, 0) AS importe, IFNULL(df.porcentaje, 0) AS porcentaje FROM ";
+		$joinsempjub = " e LEFT JOIN catsindicatos cs ON e.sindicato = cs.cve_sindicato LEFT JOIN descuentos_fijos df ON e.numero = df.numero AND df.concepto = 61";
+		$externos = "SELECT e.numero, nombre, paterno, materno, 0 AS sindicato, fec_ingre, 0 AS importe, 0 AS porcentaje FROM externos e";
 		$consulta = "";
+		$where = " WHERE (e.numero LIKE :busca OR e.nombre LIKE :busca OR e.paterno LIKE :busca OR e.materno LIKE :busca) ";
+		
+		if($this->ddlSindicato->SelectedValue != "T")
+			$where .= "AND e.sindicato = :sindicato ";
 		
 		if($this->ddlTipo->SelectedValue == 0 || $this->ddlTipo->SelectedValue == 1)
-			$consulta .= $camposempjub . "empledos" . $joinsempjub;
+			$consulta .= $camposempjub . "empleados" . $joinsempjub . $where;
 		if($this->ddlTipo->SelectedValue == 0)
 			$consulta .= " UNION ";
 		if($this->ddlTipo->SelectedValue == 0 || $this->ddlTipo->SelectedValue == 2)
-			$consulta .= $camposempjub . "pensionados" . $joinsempjub;
+			$consulta .= $camposempjub . "pensionados" . $joinsempjub . $where;
 		if($this->ddlTipo->SelectedValue == 0)
 			$consulta .= " UNION ";
 		if($this->ddlTipo->SelectedValue == 0 || $this->ddlTipo->SelectedValue == 3)
-			$consulta .= $externos;
-		
+			$consulta .= $externos . $where;
+		$consulta .= " LIMIT 1000";
 		$comando = $this->dbConexion->createCommand($consulta);
+		$comando->bindValue("busca", "%" . $this->txtNombre->Text . "%");
+		if($this->ddlSindicato->SelectedValue != "")
+			$comando->bindValue("sindicato", $this->ddlSindicato->SelectedValue);
 		$resultado = $comando->query()->readAll();
-		$this->dgDescuentos->DataSource = $resultado;
-		$this->dgDescuentos->dataBind();
+		$this->dgEmpleados->DataSource = $resultado;
+		$this->dgEmpleados->dataBind();
 	}
 }
 
