86d664853701335a75fe5eed18aadb89e0136489
itandehui <itapad@hotmail.com>
2015-07-28 02:14:37 -0500

    Agregado el importe de pensión
    Agregado el importe de pensión al rellenado de la tabla pensionados

--- copy tables.sql
+++ copy tables.sql
@@ -2,8 +2,7 @@ INSERT INTO prestamos.empleados
 SELECT cveempleado, nombre, appaterno, apmaterno, idsindicato, fechaingreso, sexo, estatus, tiponomina FROM dbprestamos.catempleado;
 
 INSERT INTO prestamos.pensionados
-SELECT cvepensionado, cvepensionado, nombre, appaterno, apmaterno, idsindicato, fechaingreso, sexo, estatus, tiponomina FROM dbprestamos.catpensionado;
+SELECT cvepensionado, cvepensionado, nombre, appaterno, apmaterno, idsindicato, fechaingreso, sexo, estatus, tiponomina, importepension FROM dbprestamos.catpensionado;
 
 INSERT INTO prestamos.externos
 SELECT idpersonaexterna, nombre, appaterno, apmaterno, CONCAT(calle, ' ', numero, ' ', colonia), curp, fechaalta, sexo, estatus FROM dbprestamos.catpersonaexterna;
-
