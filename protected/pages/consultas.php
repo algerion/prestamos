961fe866a04d714c7556980473e58047c66c86b6
itandehui <itapad@hotmail.com>
2015-07-28 15:52:36 -0500

    Página de prueba para consultas
    En esta página se probarán las consultas generadas en el archivo
    busquedas.php

---
+++ protected/pages/consultas.php
@@ -0,0 +1,24 @@
+<?php
+include_once('../compartidos/clases/conexion.php');
+include_once('/protected/Comunes/Busquedas.php');
+
+class consultas extends TPage
+{
+	var $dbConexion;
+
+	public function onLoad($param)
+	{
+		parent::onLoad($param);
+
+		$this->dbConexion = Conexion::getConexion($this->Application, "dbpr");
+		Conexion::createConfiguracion();
+
+		if(!$this->IsPostBack)
+		{
+			$resultado = Busquedas::obtenerPrestamoAnteriorSinRedocumentado($this->dbConexion, 6979);
+			print_r($resultado);
+		}
+	}
+}
+
+?>
